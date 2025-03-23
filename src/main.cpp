735492c05e787401150ef03e162ceb9e7adbec0a
Atharva Rane <atharvarane051102@gmail.com>
2025-03-23 16:18:47 +0530

    add wifi connection handling and http logging in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
-const char* SSID = SSID_SECRET;
-const char* PASSWORD = PASSWORD_SECRET;
+const char *SSID = SSID_SECRET;
+const char *PASSWORD = PASSWORD_SECRET;
 
 const int SENSOR_PIN = 34;
 
-void setup() {
+void setup()
+{
   Serial.begin(921600);
   delay(1000);
-  
+
   Serial.println("[*] initiating wifi connection sequence...");
   WiFi.begin(SSID, PASSWORD);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(":");
   }
@@ -22,9 +25,40 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   int reading = analogRead(SENSOR_PIN);
   Serial.print("[+] moisture: ");
   Serial.println(reading);
-  delay(1000);
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    http.begin("https://soggweb.vercel.app/api/logs");
+    // http.begin("http://192.168.29.184:3000/api/logs");
+
+    http.addHeader("Content-Type", "application/json");
+    // http.addHeader("Authorization", "Bearer testing...");
+
+    String payload = "{\"type\":\"upsert\",\"data\":{\"value\":" + String(reading) + "}}";
+
+    int httpResponseCode = http.POST(payload);
+    if (httpResponseCode > 0)
+    {
+      Serial.print("[+] POST success: ");
+      Serial.println(httpResponseCode);
+    }
+    else
+    {
+      Serial.print("[!] POST failed: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  }
+  else
+  {
+    Serial.println("[!] WiFi not connected!");
+  }
+
+  delay(60 * 1000); // a minute
 }
